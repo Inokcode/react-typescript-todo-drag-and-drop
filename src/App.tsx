eed993d63bc537ad9c9ee6261afaee13eaec7675
Inokcode <regilearn@gmail.com>
2022-07-13 11:10:17 +0530

    todo complested-bug need to fix in drag and drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,80 @@
-import React from "react";
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+import InputFeild from "./components/InputFeild";
+import { Todo } from "./model";
+import TodoList from "./components/TodoList";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
 //let hellofunction: (name: string) => never; //void return undifine never return any thing
 //insted of void u can use unknown
 //type can use with primitive types but interface cant use
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [CompletedTodos, setCompletedTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+  console.log(todos);
+  //
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source } = result;
+
+    console.log(result);
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    let add;
+    let active = todos;
+    let complete = CompletedTodos;
+    // Source Logic
+    if (source.droppableId === "TodosList") {
+      add = active[source.index];
+      active.splice(source.index, 1);
+    } else {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+
+    // Destination Logic
+    if (destination.droppableId === "TodosList") {
+      active.splice(destination.index, 0, add);
+    } else {
+      complete.splice(destination.index, 0, add);
+    }
+
+    setCompletedTodos(complete);
+    setTodos(active);
+  };
+  //
   return (
-    <div className="App">
-      <span className="heading">Taskify</span>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App">
+        <span className="heading">Taskify</span>
+        <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          CompletedTodos={CompletedTodos}
+          setCompletedTodos={setCompletedTodos}
+        />
+      </div>
+    </DragDropContext>
   );
 };
 
--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,4 +1,34 @@
-const InputFeild = () => {
-  return <div>InputFeild</div>;
+import { useRef } from "react";
+import "./styles.css";
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+const InputFeild: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  return (
+    <>
+      <form
+        className="input"
+        onSubmit={(e) => {
+          handleAdd(e);
+          inputRef.current?.blur();
+        }}
+      >
+        <input
+          ref={inputRef}
+          type="input"
+          placeholder="Enter a task"
+          className="input__box"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button className="input_submit" type="submit">
+          Go
+        </button>
+      </form>
+    </>
+  );
 };
 export default InputFeild;
